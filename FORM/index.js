8f8e5f619b0fcb4913ad5bf2fa50c490c69fa6ca
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-06-15 19:26:43 +0530

    Add files via upload

---
+++ FORM/index.js
@@ -0,0 +1,8 @@
+let user=document.getElementsByClassName("one")
+let pass=document.getElementsByClassName("two")
+function validate(){
+if(user.value=""){
+    document.getElementById("p1").innerHTML="empty"
+}
+return false
+}
